c16ddcb980fc0fc07c34a29000314ea6df8feb4e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-19 22:16:40 +0900

    配列の中に情報を追加（スプレッド構文）

--- noteapp-with-reatc/src/App.jsx
+++ noteapp-with-reatc/src/App.jsx
@@ -1,11 +1,23 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Main from "./components/Main.jsx";
 import './App.css';
+import {useState} from "react";
 
 const App = () => {
 
+    const [notes, setNotes] = useState([
+    ]);
+    
     const onAddNote = () => {
       console.log("追加ボタンの押下");
+      const newNote = {
+          id: 1,
+          title: "新しいノート",
+          content: "内容",
+          modDate: Date.now(),
+      };
+      setNotes([...notes, newNote]);
+      console.log(notes);
     };
 
   return (
