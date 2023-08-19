527a29b2cea3635e57841c3c8ed9ae36a8c6ea1d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-19 21:42:12 +0900

    CSSでスタイリング

--- noteapp-with-reatc/src/components/Sidebar.jsx
+++ noteapp-with-reatc/src/components/Sidebar.jsx
@@ -5,8 +5,21 @@ import './Sidebar.css';
 class Sidebar extends Component {
     render() {
         return (
-            <div className="app-sodebar">
-                <div className="app-sidebar-"></div>
+            <div className="app-sidebar">
+                <div className="app-sidebar-header">
+                    <h1>ノート</h1>
+                    <button>追加</button>
+                </div>
+                <div className="app-sidebar-notes">
+                    <div className="app-sidebar-note">
+                        <div className="sidebar-note-title">
+                            <strong>タイトル</strong>
+                            <button>削除</button>
+                        </div>
+                        <p>ノートの内容です。</p>
+                        <small>最後の修正日：XX/XX</small>
+                    </div>
+                </div>
             </div>
         );
     }
