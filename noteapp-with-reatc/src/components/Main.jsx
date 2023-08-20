7c821eeaee9438ecfcb491b2e790ebb57546941e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-20 22:18:43 +0900

    Mainコンポーネント側に noteの情報を渡す

--- noteapp-with-reatc/src/components/Main.jsx
+++ noteapp-with-reatc/src/components/Main.jsx
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import './Main.css';
 
-const  Main = () => {
+const  Main = ({activeNote}) => {
+
+    if (!activeNote) {
+        return (<p>ノートが選択されておりません</p>)
+    }
+
+
         return (
             <div className="app-main">
                 <div className="app-main-note-edit">
@@ -9,8 +15,8 @@ const  Main = () => {
                     <textarea id="" placeholder="ノート内容を記入"></textarea>
                 </div>
                 <div className="app-main-note-preview">
-                    <h1 className="preview-title">タイトル</h1>
-                    <div className="markdown-preview">ノート内容</div>
+                    <h1 className="preview-title">{activeNote.title}</h1>
+                    <div className="markdown-preview">{activeNote.content}</div>
                 </div>
             </div>
         );
