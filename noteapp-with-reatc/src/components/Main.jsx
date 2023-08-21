54893ea9700bd2459ee36379b63245fe274fc0aa
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-21 23:26:52 +0900

    Revert "テスト"
    This reverts commit 9c407c2b25e75fd1d9737057a8e1220b53b38cdb.

---
+++ noteapp-with-reatc/src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './Main.css';
+
+const  Main = ({activeNote, onUpdateNote}) => {
+
+    const onEditNote = (key, value) => {
+        onUpdateNote({
+            id: activeNote.id,
+            [key]: value,
+            modDate: Date.now(),
+        })
+
+    };
+
+
+    if (!activeNote) {
+        return (<p　className="no-active-note">ノートが選択されておりません</p>)
+    }
+
+
+        return (
+            <div className="app-main">
+                <div className="app-main-note-edit">
+                    <input id="title" type="text" value={activeNote.title} onChange={(e) => onEditNote("title", e.target.value)}/>
+                    <textarea id="content" placeholder="ノート内容を記入" value={activeNote.content} onChange={(e) => onEditNote("content", e.target.value)}></textarea>
+                </div>
+                <div className="app-main-note-preview">
+                    <h1 className="preview-title">{activeNote.title}</h1>
+                    <div className="markdown-preview">{activeNote.content}</div>
+                </div>
+            </div>
+        );
+}
+
+Main.propTypes = {};
+
+export default Main;
