fe7338ea1bd721e25f4e5aca28475b98a06f57a8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-21 23:46:59 +0900

    ライブラリが使えるようにする

--- noteapp-with-reatc/src/components/Main.jsx
+++ noteapp-with-reatc/src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Main.css';
+import ReactMarkdown from "react-markdown";
 
 const  Main = ({activeNote, onUpdateNote}) => {
 
@@ -26,7 +27,7 @@ const  Main = ({activeNote, onUpdateNote}) => {
                 </div>
                 <div className="app-main-note-preview">
                     <h1 className="preview-title">{activeNote.title}</h1>
-                    <div className="markdown-preview">{activeNote.content}</div>
+                    <ReactMarkdown className="markdown-preview">{activeNote.content}</ReactMarkdown>
                 </div>
             </div>
         );
